54319f386247ed73885194751e22448324b6eff0
Fer <fernandoza@gmail.com>
2020-06-02 11:32:03 -0500

    feat: create a params example

---
+++ src/main/java/ShowStudentServlet.java
@@ -0,0 +1,29 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "ShowStudentServlet", urlPatterns = "/users")
+public class ShowStudentServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String name = request.getParameter("name");
+        out.println("<h3>Hello " + name + "</h3>");
+
+        try{
+            int grade = Integer.parseInt(request.getParameter("grade"));
+            out.println("<span>Grade: " + grade + ".00</span>");
+        }catch (NumberFormatException e){
+            e.printStackTrace();
+            out.println("Grade not present");
+        }
+
+    }
+
+}
