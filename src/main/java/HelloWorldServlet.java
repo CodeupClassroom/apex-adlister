dcbea17647460a6e350a8f844a5cff0b9df94899
Fer <fernandoza@gmail.com>
2020-06-08 09:53:05 -0500

    feat: create a connection between a controller and view

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,12 +1,30 @@
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/")
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.getWriter().println("<h1>Hello, World!</h1>");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String name = request.getParameter("name");
+        String flavor = "red";
+
+        if(name == null) {
+            flavor = "green";
+            name = "World";
+        } else if ( name.equals("fer")){
+            flavor = "white";
+        } else if (name.equals("bgates")) {
+            response.sendRedirect("https://microsoft.com");
+            return;
+        }
+
+        // these lines creates fields in the request to be used in the front end (Views)
+        request.setAttribute("name", name.toUpperCase());
+        request.setAttribute("tea", flavor);
+        // connects the controller with view
+        request.getRequestDispatcher("/hello.jsp").forward(request, response);
     }
 }
