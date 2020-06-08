a8b9a23ded569c81bfa5d93fa0e754fe724162ec
Fer <fernandoza@gmail.com>
2020-06-08 11:54:35 -0500

    feat: create the insert feature with a controller and a view

--- src/main/java/controllers/CreateProductServlet.java
+++ src/main/java/controllers/CreateProductServlet.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import daos.DaoFactory;
+import models.Product;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,8 +13,14 @@ import java.io.IOException;
 @WebServlet(name = "CreateProductServlet", urlPatterns = "/products/create")
 public class CreateProductServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        double price = Double.parseDouble(request.getParameter("price"));
 
-        System.out.println("post works");
+        Product newProduct = new Product();
+        newProduct.setName(name);
+        newProduct.setPrice(price);
+
+        DaoFactory.getProductsDao().insert(newProduct);
 
     }
 
