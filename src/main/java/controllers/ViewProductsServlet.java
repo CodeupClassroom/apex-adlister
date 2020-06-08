e24d127e2babafb81d21e77be5ca1da553c86978
Fer <fernandoza@gmail.com>
2020-06-08 11:30:20 -0500

    feat: create a servlet that handles the login and the quest and display the date in the View

---
+++ src/main/java/controllers/ViewProductsServlet.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import daos.DaoFactory;
+import interfaces.Products;
+import models.Product;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "ViewProductsServlet", urlPatterns = "/products")
+public class ViewProductsServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        Products products = DaoFactory.getProductsDao();
+        List<Product> productList = products.all();
+        request.setAttribute("products", productList);
+        request.getRequestDispatcher("/products/index.jsp").forward(request, response);
+    }
+}
---
+++ src/main/webapp/products/index.jsp
@@ -0,0 +1,33 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%--
+  Created by IntelliJ IDEA.
+  User: fer
+  Date: 6/8/20
+  Time: 11:18 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>All products</title>
+</head>
+<body>
+
+<h1>All products</h1>
+
+<table border="1">
+    <tr>
+        <th>Name</th>
+        <th>Price</th>
+    </tr>
+    <c:forEach var="product" items="${products}">
+        <tr>
+            <td>${product.name}</td>
+            <td>${product.price}</td>
+        </tr>
+    </c:forEach>
+</table>
+
+</body>
+</html>
